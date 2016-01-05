596cff7809fac4aeb1e76a4b87143ce6ff5007d2
Stacey Sugiono <staceysugiono@gmail.com>
2016-01-05 15:55:17 -0800

    merge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
 
-<<<<<<< HEAD
-    # Returns the full title on a per-page basis.
-=======
-  # Returns the full title on a per-page basis.
->>>>>>> filling-in-layout
+# Returns the full title on a per-page basis.
   def full_title(page_title = '')
     base_title = "Flow Finder"
     if page_title.empty?
