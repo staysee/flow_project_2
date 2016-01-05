9d2d735b41a63fddc4f9b48efe5aba40b09bacdf
Stacey Sugiono <staceysugiono@gmail.com>
2016-01-05 02:09:38 -0800

    update user model, secure password validations, tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  has_secure_password
+  before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -9,5 +9,6 @@ class User < ActiveRecord::Base
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
 
+  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,8 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com"), password: "foobar", password_confirmation: "foobar")
+
   end
 
   test "should be valid" do
@@ -20,6 +21,11 @@ class UserTest < ActiveSupport::TestCase
 
   test "name should not be too long" do
     @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
     assert_not @user.valid?
   end
 
@@ -56,4 +62,14 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+# Minimum Password Standards
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
