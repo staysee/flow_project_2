575d123b31836dfc852c5aeffd5f1a7ae47d083f
Stacey Sugiono <staceysugiono@gmail.com>
2016-01-05 12:00:24 -0800

    set up event model - index and show view page, route, controller

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   #route to post user form to
   post '/' => 'users#create'
 
+  resources :users
+
 
   # SESSIONS ROUTES
   #display form
@@ -28,5 +30,10 @@ Rails.application.routes.draw do
 
   get 'targets/' => 'targets#index', as: :targets
 
-  resources :users
+
+  # EVENTS ROUTES
+  #route to events index page - list of all events
+  get 'events/' => 'events#index', as: :events
+  #route to individual event page
+  get 'events/:id' => 'events#show', as: :event
 end
