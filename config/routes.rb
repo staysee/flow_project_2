c512beadd576885e74b6c915f47ca465c4be3368
Stacey Sugiono <staceysugiono@gmail.com>
2016-01-05 01:29:34 -0800

    added routes to the layouts, created contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  # STATIC PAGE ROUTES
   #route for our home page
   root 'static_pages#home'
   #route for an about page
-  get 'static_pages/about'
+  get 'static_pages/about' => 'static_pages#about', as: :about
+  get 'static_pages/contact' => 'static_pages#contact', as: :contact
 
+  # USERS ROUTES
   #route to users index page - list of all users
   get 'users/' => 'users#index', as: :users
   #route to users sign up form
@@ -15,11 +18,11 @@ Rails.application.routes.draw do
   post '/' => 'users#create'
 
 
+  # SESSIONS ROUTES
   #display form
   get 'sessions/new' => 'sessions#new', as: :new_session
   #create a new session (this is the login post)
   post 'sessions/new' => 'sessions#create', as: :create_session
-
   #destroy the session and log a user out
   get 'sessions/destroy' => 'sessions#destroy', as: :destroy_session
 
