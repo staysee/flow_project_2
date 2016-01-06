5e82a57c3e357aed8863fe12c3f4ae4471cfbab8
Stacey Sugiono <staceysugiono@gmail.com>
2016-01-06 10:14:25 -0800

    adding site authorization, only user can edit own page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @users = User.all
@@ -45,6 +47,20 @@ class UsersController < ApplicationController
 private
   def user_params
     params.require(:user).permit(:name, :handle, :email, :password, :password_confirmation)
+  end
+
+  #Confirms a logged-in user
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to new_session_path
+    end
+  end
+
+  #Confirms the correct user
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 
 end
