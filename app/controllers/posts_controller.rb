c6dd13b6479d50f55f48aa9d258a50aac3b28722
Stacey Sugiono <staceysugiono@gmail.com>
2016-01-06 16:05:49 -0800

    all posts show up in columns on post index page, handles of post link to user page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def index
+    @posts = Post.all
+  end
+
+  def show
   end
 
   def create
