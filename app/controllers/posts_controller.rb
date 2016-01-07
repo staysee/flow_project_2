9cc81a7c26e73e4539a2911872fb184de8f0877d
Stacey Sugiono <staceysugiono@gmail.com>
2016-01-06 20:04:32 -0800

    current user can create new posts to their own profile page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,10 +13,25 @@ class PostsController < ApplicationController
   end
 
   def create
-
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      flash[:success] = 'Posted!'
+      redirect_to user_path(current_user)
+    else
+      flash.now[:error] = 'Error. Not posted. Try again.'
+      redirect_to :back
+    end
   end
 
   def destroy
+  end
+
+
+private
+
+  def post_params
+    params.require(:post).permit(:body, :image_url)
   end
 
 
