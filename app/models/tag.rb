dbf8db6da478a4c2ddcbfd1c826ecd403e654a86
Stacey Sugiono <staceysugiono@gmail.com>
2016-01-08 00:16:29 -0800

    fixed stack level too deep error, could not destroy

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ActiveRecord::Base
   # attr_accessible :name
-  has_many :taggings, dependent: :destroy
-  has_many :posts, through: :taggings,  dependent: :destroy
+  has_many :taggings
+  has_many :posts, through: :taggings
 
   def self.counts
     self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id, tags.name")
